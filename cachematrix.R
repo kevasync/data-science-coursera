2167f1c4e7451b7f352b5bb3735c7592e5cf63bc
kevin.tinn <kevin.tinn@asynchrony.com>
2016-11-11 10:32:48 -0600

    solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## wrote some functions to create caching matrix object
+## as well as  function to calculate matrix inverse and cache it using aforementioned object
 
-## Write a short comment describing this function
+## this function creates object cacehs matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(m) {
+    x <<- m
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## comments, really?  playoffs!?
+## this function calculates the inverse from a caching matrix object and writes the inverse to the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invertedX <- x$getinverse()
+  if(!is.null(invertedX)){
+    return(invertedX)
+  }
+
+  m <- x$get()
+  dims <- dim(m)
+  invertedX <- matrix(nrow=0, ncol=dims[1])
+  for(j in 1:dims[2]) {
+    invertedX <- rbind(invertedX, m[,j])
+  }
+
+  x$setinverse(invertedX)
+  invertedX
 }
