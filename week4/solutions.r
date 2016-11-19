29a9f1cf971654be0aeb9236de1ab5d7b7d373f3
kevin.tinn <kevin.tinn@asynchrony.com>
2016-11-19 14:48:59 -0600

    refactor

--- week4/solutions.r
+++ week4/solutions.r
@@ -1,20 +1,18 @@
-best = function(state, reason) {
+best = function(state, outcome) {
   outcomes = read.csv("outcome-of-care-measures.csv")
   
   if(!is.element(state, unique(outcomes$State))) stop('invalid state')
   
-  sourceDataColIdx = 
-    if(reason == 'heart attack') 11
-    else if (reason == 'heart failure') 17
-    else if (reason == 'pneumonia') 23
+  nameIdx <- 2
+  dataIdx = 
+    if(outcome == 'heart attack') 11
+    else if (outcome == 'heart failure') 17
+    else if (outcome == 'pneumonia') 23
     else stop("invalid outcome")
   
-  sourceNameColIdx <- 2
-  nameDataKvps = outcomes[which(outcomes$State == state & outcomes[sourceDataColIdx] != "Not Available"), 
-                          c(sourceNameColIdx, sourceDataColIdx)]
+  filteredData = outcomes[which(outcomes$State == state & outcomes[dataIdx] != "Not Available"), 
+                          c(nameIdx, dataIdx)]
   
-  keyIdx = 1
-  valueIdx = 2
-  minIdxs = which.min(as.numeric(as.character(nameDataKvps[,valueIdx])))
-  as.character(sort(nameDataKvps[minIdxs, keyIdx])[1])
+  minIdxs = which.min(as.numeric(as.character(filteredData[,2])))
+  as.character(head(sort(filteredData[minIdxs, 1]), 1))
 } 
