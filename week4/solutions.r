3e940327cad9f2fd55871526751df2415b735020
kevin.tinn <kevin.tinn@asynchrony.com>
2016-11-19 17:19:17 -0600

    refactored; started on rank all

--- week4/solutions.r
+++ week4/solutions.r
@@ -1,30 +1,49 @@
 best = function(state, outcome) {
   rawData = readOutcomeMeasures()
-  d = sortedNameAndOutcome(rawData, state, outcome)
+  d = sortedData(rawData, outcome, state)
   as.character(d$name[1])
 } 
 
-rankHospital = function(state, outcome, rank){
+rankHospital = function(state, outcome, rank = "best"){
   rawData = readOutcomeMeasures()
-  d = sortedNameAndOutcome(rawData, state, outcome)
+  d = sortedData(rawData, outcome, state)
+  
+  rank = getRank(d, rank)
   
   if(nrow(d) < rank) NA
   else as.character(d$name[rank])
 }
 
-sortedNameAndOutcome = function(rawData, state, outcome) {
+rankAll = function (rank = "best") {
+  rawData = readOutcomeMeasures()
+  d = sortedData(rawData, outcome)
+  
+  rank = getRank(d, rank)
+  
+  if(nrow(d) < rank) NA
+  else as.character(d$name[rank])
+}
+
+getRank = function(d, rank) {
+  if(rank == "best") 1
+  else if(rank == "worst") nrow(d)
+  else rank
+}
+
+sortedData = function(rawData, outcome, state = NA) {
   if(!is.element(state, rawData$State)) stop('invalid state')
   
   nameIdx = 2
+  stateIdx = 7
   dataIdx = if(outcome == 'heart attack') 11
             else if (outcome == 'heart failure') 17
             else if (outcome == 'pneumonia') 23
             else stop("invalid outcome")
   
-  filtered = rawData[which(rawData$State == state & rawData[dataIdx] != "Not Available"), 
-          c(nameIdx, dataIdx)]
+  filtered = rawData[which((is.na(state) | rawData$State == state) & rawData[dataIdx] != "Not Available"), 
+          c(nameIdx, dataIdx, stateIdx)]
   
-  names(filtered) = c("name", "value")
+  names(filtered) = c("name", "value", "state")
   
   #convert data to numeric without truncation
   filtered[, 2] = as.numeric(as.character(filtered$value))
