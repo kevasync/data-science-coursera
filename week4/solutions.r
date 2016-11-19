2a621bc3abcc54c4f238b0c82b40b977dc3ef135
kevin.tinn <kevin.tinn@asynchrony.com>
2016-11-19 15:15:50 -0600

    created rankHospital and refactored common functionality

--- week4/solutions.r
+++ week4/solutions.r
@@ -1,18 +1,38 @@
 best = function(state, outcome) {
-  outcomes = read.csv("outcome-of-care-measures.csv")
+  rawData = readOutcomeMeasures()
+  filteredData = getHospitalNameAndData(rawData, state, outcome)
+
+  minIdxs = which.min(as.numeric(as.character(filteredData[, 2])))
+  as.character(head(sort(filteredData[minIdxs, 1]), 1))
+} 
+
+rankHospital = function(state, outcome, rank){
+  rawData = readOutcomeMeasures()
+  filteredData = getHospitalNameAndData(rawData, state, outcome)
+}
+
+
+
+getHospitalNameAndData = function(rawData, state, outcome) {
+  validateState(state, rawData)
+  nameIdx = 2
+  dataIdx = getDataColumnIndex(outcome)
+  return(rawData[which(rawData$State == state & rawData[dataIdx] != "Not Available"), 
+          c(nameIdx, dataIdx)])
   
-  if(!is.element(state, unique(outcomes$State))) stop('invalid state')
+  validateState = function(state, outcomes) {
+    if(!is.element(state, outcomes$State)) stop('invalid state')
+  }
   
-  nameIdx <- 2
-  dataIdx = 
+  getDataColumnIndex = function(outcome) {
     if(outcome == 'heart attack') 11
     else if (outcome == 'heart failure') 17
     else if (outcome == 'pneumonia') 23
     else stop("invalid outcome")
-  
-  filteredData = outcomes[which(outcomes$State == state & outcomes[dataIdx] != "Not Available"), 
-                          c(nameIdx, dataIdx)]
-  
-  minIdxs = which.min(as.numeric(as.character(filteredData[,2])))
-  as.character(head(sort(filteredData[minIdxs, 1]), 1))
-} 
+  }
+}
+
+readOutcomeMeasures = function() {
+  read.csv("outcome-of-care-measures.csv")
+}
+
