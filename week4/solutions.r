69a5f20ee4364f6be1fe45772d07160ab0b1f7a8
kevin.tinn <kevin.tinn@asynchrony.com>
2016-11-19 18:06:12 -0600

    done with rankall

--- week4/solutions.r
+++ week4/solutions.r
@@ -8,30 +8,30 @@ rankHospital = function(state, outcome, rank = "best"){
   rawData = readOutcomeMeasures()
   d = sortedData(rawData, outcome, state)
   
-  rank = getRank(d, rank)
-  
-  if(nrow(d) < rank) NA
-  else as.character(d$name[rank])
-}
-
-rankAll = function (rank = "best") {
-  rawData = readOutcomeMeasures()
-  d = sortedData(rawData, outcome)
-  
-  rank = getRank(d, rank)
-  
-  if(nrow(d) < rank) NA
-  else as.character(d$name[rank])
-}
-
-getRank = function(d, rank) {
-  if(rank == "best") 1
+  rank = if(rank == "best") 1
   else if(rank == "worst") nrow(d)
   else rank
+  
+  if(nrow(d) < rank) NA
+  else as.character(d$name[rank])
 }
 
-sortedData = function(rawData, outcome, state = NA) {
-  if(!is.element(state, rawData$State)) stop('invalid state')
+rankAll = function (outcome, rank = "best") {
+  rawData = readOutcomeMeasures()
+  d = sortedData(rawData, outcome, descending = rank == "worst")
+   
+  if(is.numeric(rank)) {
+    nRows = nrow(d)
+    if(rank > nRows) return(NA)
+    
+    d = d[rank:length(d), ]
+  }
+  
+  d[,c(1,3)]
+}
+
+sortedData = function(rawData, outcome, state = NA, descending = FALSE) {
+  if(!is.na(state) & !is.element(state, rawData$State)) stop('invalid state')
   
   nameIdx = 2
   stateIdx = 7
@@ -48,7 +48,7 @@ sortedData = function(rawData, outcome, state = NA) {
   #convert data to numeric without truncation
   filtered[, 2] = as.numeric(as.character(filtered$value))
   
-  filtered[order(filtered$value, filtered$name), ]
+  filtered[order(filtered$value, filtered$name, decreasing = descending), ]
 }
 
 readOutcomeMeasures = function() {
