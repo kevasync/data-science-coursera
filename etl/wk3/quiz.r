3d33fc711350a73b34e05953e69da30d2dc0fc6e
kevin.tinn <kevin.tinn@asynchrony.com>
2016-12-13 20:44:28 -0600

    wk3, #3

--- etl/wk3/quiz.r
+++ etl/wk3/quiz.r
@@ -1,7 +1,24 @@
 library(dplyr)
+library(jpeg)
+library(dplyr)
 
 #1
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile = "idahoHousingData.csv")
 idahoHousingData = tbl_cube(read_csv("idahoHousingData.csv"))
 meetsCriteria = mutate(idahoHousingData, x = AGS == 6 & ACR == 3) %>% select(x)
 head(which(meetsCriteria$x), 3)
+
+#2
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg", destfile = "instructorPic.jpg")
+pic = readJPEG("instructorPic.jpg", native = T)
+c(quantile(pic, probs = .30), quantile(pic, probs = .80))
+
+#3
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", destfile = "gdpData.csv")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", destfile = "eduData.csv")
+eduData = read_csv("eduData.csv")
+gdpData = read_csv("gdpData.csv")
+cleanedEduData = mutate(eduData, Name = `Long Name` ) %>% select (CountryCode, Name)
+cleanedGdpData = mutate(gdpData, CountryCode = X1, GDP = as.numeric(`Gross domestic product 2012`)) %>% filter(!is.na(CountryCode) & !is.na(GDP)) %>% select(CountryCode, GDP)
+joined = inner_join(cleanedEduData, cleanedGdpData) %>% arrange(desc(GDP))
+c(dim(joined)[1], joined$Name[13])
